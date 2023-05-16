45e572de149bb34b1ca3cffb551b58cc234de256
Czino <eric@czino.de>
2023-05-16 15:22:38 +0200

    Add i18n to templates

--- pug.config.js
+++ pug.config.js
@@ -8,9 +8,12 @@ const {
   IS_DEV
 } = require('./helpers')
 
+const i18n = require('./i18n')
+
 module.exports = {
   basedir: './src/includes',
   siteData: require('./site-data'),
+  i18n,
   getUrl,
   assetUrl,
   assetPath,
