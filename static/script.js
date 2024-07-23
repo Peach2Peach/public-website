44cab84511846e5aee569108ac9cc1c7f7e2aa81
viva <viva@peachbitcoin.com>
2024-07-23 20:59:47 +0200

    Push Update

--- static/script.js
+++ static/script.js
@@ -18,17 +18,41 @@ window.addEventListener('load', async function() {
     console.log('Libreria marked.js caricata correttamente come funzione.');
 
     const lang = navigator.language || 'en';
-    const contentPath = `/${lang}/index.html`; // Assicurati che questo percorso sia corretto
+    const contentPaths = [`/${lang}/index.html`]; // Assicurati che questo percorso sia corretto
 
-    try {
-      const response = await fetch(contentPath);
-      const text = await response.text();
-      // Converti il contenuto Markdown in HTML
-      const htmlContent = markdownFunction(text);
-      // Inietta l'HTML nel div con id "content"
-      document.getElementById('content').innerHTML = htmlContent;
-    } catch (error) {
-      console.error('Errore nel caricamento del file Markdown:', error);
+    // Aggiungi i percorsi principali per tutte le lingue disponibili
+    const MAIN_PATHS = [
+      '/blog',
+      '/for-meetups',
+      '/for-businesses/',
+      '/peach-for-businesses/',
+      '/support/',
+      '/buy-btc-with-cash/',
+      '/how-it-works/',
+      '/buy-btc-with-cash/',
+      '/how-to-buy-btc-no-kyc/'
+    ];
+    const LANGUAGES = ['es', 'de', 'it', 'fr', 'el', 'hu', 'nl', 'pl', 'pt', 'sw', 'uk'];
+    
+    MAIN_PATHS.forEach(path => {
+      LANGUAGES.forEach(lang => {
+        contentPaths.push(`/${lang}${path}`);
+      });
+    });
+
+    for (const contentPath of contentPaths) {
+      try {
+        const response = await fetch(contentPath);
+        const text = await response.text();
+        // Converti il contenuto Markdown in HTML
+        const htmlContent = markdownFunction(text);
+        // Crea un div per ogni contenuto e iniettalo nel body
+        const contentDiv = document.createElement('div');
+        contentDiv.innerHTML = htmlContent;
+        document.body.appendChild(contentDiv);
+      } catch (error) {
+        console.error(`Errore nel caricamento del file Markdown da ${contentPath}:`, error);
+      }
     }
   } else {
     console.error('La libreria marked.js non è stata caricata correttamente.');
