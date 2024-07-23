482b48729f913ce12e8efb3cb4e7c2dc56978a60
viva <viva@peachbitcoin.com>
2024-07-23 15:25:41 +0200

    handling script update 2

--- static/service-worker.js
+++ static/service-worker.js
@@ -9,7 +9,6 @@ const FILES_TO_CACHE = [
   '/fonts/baloo-2-v16-latin-500.woff',
   '/fonts/baloo-2-v16-latin-regular.woff2',
   '/fonts/baloo-2-v16-latin-600.woff2',
-  '/static/css',
   '../src/css/main.css',
   '/fonts/baloo-2-v16-latin-800.woff2',
   '/fonts/baloo-2-v16-latin-500.woff2',
@@ -23,62 +22,98 @@ const FILES_TO_CACHE = [
   '/img/favicon/android-chrome-512x512.png'
 ];
 
-// Evento di installazione del Service Worker
 self.addEventListener('install', function(event) {
-  // Mettere i file in cache durante l'installazione
+  console.log('[ServiceWorker] Installazione in corso...');
+
+  
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
+      console.log('[ServiceWorker] Caching dei file statici in corso...');
       return cache.addAll(FILES_TO_CACHE);
     }).catch(function(error) {
-      // Gestione dell'errore di caching durante l'installazione
+      console.error('Errore durante il caching dei file:', error);
     })
   );
+
+  // Forza il nuovo Service Worker a diventare attivo subito
+  self.skipWaiting();
 });
 
 // Evento di attivazione del Service Worker
 self.addEventListener('activate', function(event) {
+  console.log('[ServiceWorker] Attivazione in corso...');
+
   // Pulire le vecchie cache non utilizzate
   event.waitUntil(
     caches.keys().then(function(keyList) {
       return Promise.all(keyList.map(function(key) {
         if (key !== CACHE_NAME) {
+          console.log('[ServiceWorker] Rimozione della cache vecchia:', key);
           return caches.delete(key);
         }
       }));
+    }).catch(function(error) {
+      console.error('Errore durante la pulizia delle cache:', error);
     })
   );
-  // Prendere il controllo delle pagine attualmente aperte
+
+  // check actual open page
   return self.clients.claim();
 });
 
 // Evento di fetch (richiesta di risorse)
 self.addEventListener('fetch', function(event) {
-  // Ignorare richieste non GET
+  console.log('[ServiceWorker] Fetching:', event.request.url);
+
+  // ignore non GET rqst
   if (event.request.method !== 'GET') {
     return;
   }
 
-  // Gestire solo le richieste HTTP
-  if (event.request.url.startsWith('http')) {
+  // check if rqst is correct
+  if (event.request.headers.get('accept').includes('text/html')) {
+    // cache-then-network
     event.respondWith(
-      caches.match(event.request).then(function(response) {
-        // Restituire la risposta dalla cache o effettuare una richiesta in rete
-        return response || fetch(event.request).then(function(networkResponse) {
-          // Controllare se la risposta della rete è valida
-          if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
-            return networkResponse;
-          }
-          // Clonare la risposta della rete e metterla in cache
-          let responseToCache = networkResponse.clone();
+      caches.match(event.request).then(function(cachedResponse) {
+        if (cachedResponse) {
+          // response to cache
+          return cachedResponse;
+        }
+
+        // Fetch to put in intranet
+        return fetch(event.request).then(function(networkResponse) {
           caches.open(CACHE_NAME).then(function(cache) {
-            cache.put(event.request, responseToCache).catch(function(error) {
-              // Gestione dell'errore durante il caching della richiesta
+            cache.put(event.request, networkResponse.clone()).catch(function(error) {
+              console.error('Errore durante il caching della risposta di rete:', error);
+            });
+          });
+          return networkResponse;
+        }).catch(function(error) {
+          // error while fetch network
+          console.error('Errore durante il fetch di rete:', error);
+
+          // fallback case of error
+          return caches.match('/offline.html');
+        });
+      }).catch(function(error) {
+        console.error('Errore durante il fetch dalla cache:', error);
+        return caches.match('/offline.html');
+      })
+    );
+  } else {
+    // for CSS, JS, immagini, ecc
+    event.respondWith(
+      caches.match(event.request).then(function(cachedResponse) {
+        return cachedResponse || fetch(event.request).then(function(networkResponse) {
+          caches.open(CACHE_NAME).then(function(cache) {
+            cache.put(event.request, networkResponse.clone()).catch(function(error) {
+              console.error('Errore durante il caching della risposta di rete:', error);
             });
           });
           return networkResponse;
         });
       }).catch(function(error) {
-        // Gestione dell'errore durante il fetch e caching
+        console.error('Errore durante il fetch dalla cache:', error);
       })
     );
   }
