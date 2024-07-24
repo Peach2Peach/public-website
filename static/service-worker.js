934e7420410952f7f7c876f959a1ac710af60cbf
viva <viva@peachbitcoin.com>
2024-07-24 09:54:22 +0200

    service worker edit

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,98 +1,73 @@
-const CACHE_NAME = 'my-pwa-cache-v1';
+// Nome della cache
+const CACHE_NAME = 'static-v1';
+
+// File da mettere in cache
 const FILES_TO_CACHE = [
   '/',
   '/index.html',
-  '/_headers',
-  '/fonts/baloo-2-v16-latin-500.woff',
-  '/fonts/baloo-2-v16-latin-regular.woff2',
-  '/fonts/baloo-2-v16-latin-600.woff2',
-  '../src/css/main.css',
-  '/fonts/baloo-2-v16-latin-800.woff2',
-  '/fonts/baloo-2-v16-latin-500.woff2',
-  '/static/baloo-2-v16-latin-600.woff',
-  '/_redirects',
   '/script.js',
   '/css/main.css',
   '/js/main.js',
-  '/site.webmanifest',
-  '/img/favicon/android-chrome-192x192.png',
-  '/img/favicon/android-chrome-512x512.png'
+  '/site.webmanifest'
 ];
 
-const MAIN_PATHS = [
-  '/blog',
-  '/for-meetups',
-  '/for-businesses/',
-  '/peach-for-businesses/',
-  '/support/',
-  '/buy-btc-with-cash/',
-  '/how-it-works/',
-  '/buy-btc-with-cash/',
-  '/how-to-buy-btc-no-kyc/'
-];
-
-const LANGUAGES = ['es', 'de', 'it', 'fr', 'el', 'hu', 'nl', 'pl', 'pt', 'sw', 'uk'];
-
-// Installazione del Service Worker
-self.addEventListener('install', event => {
+// Evento di installazione del Service Worker
+self.addEventListener('install', function(event) {
+  // Mettere i file in cache durante l'installazione
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Caching files during install');
-        return cache.addAll(FILES_TO_CACHE);
-      })
-  );
-  self.skipWaiting();
-});
-
-// Attivazione del Service Worker
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            console.log('Deleting old cache:', cache);
-            return caches.delete(cache);
-          }
-        })
-      );
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(FILES_TO_CACHE);
+    }).catch(function(error) {
+      // Gestione dell'errore di caching durante l'installazione
     })
   );
-  self.clients.claim();
 });
 
-// Intercettazione delle richieste di rete
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response; // Se il file è nella cache, restituiscilo
+// Evento di attivazione del Service Worker
+self.addEventListener('activate', function(event) {
+  // Pulire le vecchie cache non utilizzate
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
         }
-        return fetch(event.request).then(
-          function(response) {
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-            var responseToCache = response.clone();
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-            return response;
-          }
-        );
-      })
-  );
-});
-
-// Funzione per mettere in cache le pagine principali e le lingue disponibili
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      const pathsToCache = MAIN_PATHS.flatMap(path => LANGUAGES.map(lang => `/${lang}${path}`));
-      return cache.addAll(pathsToCache);
+      }));
     })
   );
-}); 
+  // Prendere il controllo delle pagine attualmente aperte
+  return self.clients.claim();
+});
+
+// Evento di fetch (richiesta di risorse)
+self.addEventListener('fetch', function(event) {
+  // Ignorare richieste non GET
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  // Gestire solo le richieste HTTP
+  if (event.request.url.startsWith('http')) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        // Restituire la risposta dalla cache o effettuare una richiesta in rete
+        return response || fetch(event.request).then(function(networkResponse) {
+          // Controllare se la risposta della rete è valida
+          if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
+            return networkResponse;
+          }
+          // Clonare la risposta della rete e metterla in cache
+          let responseToCache = networkResponse.clone();
+          caches.open(CACHE_NAME).then(function(cache) {
+            cache.put(event.request, responseToCache).catch(function(error) {
+              // Gestione dell'errore durante il caching della richiesta
+            });
+          });
+          return networkResponse;
+        });
+      }).catch(function(error) {
+        // Gestione dell'errore durante il fetch e caching
+      })
+    );
+  }
+});
