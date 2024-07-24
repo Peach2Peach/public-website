ee20227f3be0d340e8074d6f6d265cea84cd25df
viva <viva@peachbitcoin.com>
2024-07-24 18:40:10 +0200

    caching blog 2

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,4 +1,7 @@
+// Nome della cache
 const CACHE_NAME = 'static-v1';
+
+// File da mettere in cache
 const FILES_TO_CACHE = [
   '/',
   '/index.html',
@@ -23,15 +26,19 @@ const FILES_TO_CACHE = [
 
 // Evento di installazione del Service Worker
 self.addEventListener('install', function(event) {
+  // Mettere i file in cache durante l'installazione
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll(FILES_TO_CACHE);
+    }).catch(function(error) {
+      // Gestione dell'errore di caching durante l'installazione
     })
   );
 });
 
 // Evento di attivazione del Service Worker
 self.addEventListener('activate', function(event) {
+  // Pulire le vecchie cache non utilizzate
   event.waitUntil(
     caches.keys().then(function(keyList) {
       return Promise.all(keyList.map(function(key) {
@@ -41,6 +48,7 @@ self.addEventListener('activate', function(event) {
       }));
     })
   );
+  // Prendere il controllo delle pagine attualmente aperte
   return self.clients.claim();
 });
 
@@ -55,24 +63,23 @@ self.addEventListener('fetch', function(event) {
   if (event.request.url.startsWith('http')) {
     event.respondWith(
       caches.match(event.request).then(function(response) {
-        if (response) {
-          return response;
-        }
-
-        return fetch(event.request).then(function(networkResponse) {
+        // Restituire la risposta dalla cache o effettuare una richiesta in rete
+        return response || fetch(event.request).then(function(networkResponse) {
+          // Controllare se la risposta della rete è valida
           if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
             return networkResponse;
           }
-
+          // Clonare la risposta della rete e metterla in cache
           let responseToCache = networkResponse.clone();
           caches.open(CACHE_NAME).then(function(cache) {
-            cache.put(event.request, responseToCache);
+            cache.put(event.request, responseToCache).catch(function(error) {
+              // Gestione dell'errore durante il caching della richiesta
+            });
           });
-
           return networkResponse;
-        }).catch(function() {
-          return caches.match('/offline.html'); // Servire un file di fallback offline se disponibile
         });
+      }).catch(function(error) {
+        // Gestione dell'errore durante il fetch e caching
       })
     );
   }
